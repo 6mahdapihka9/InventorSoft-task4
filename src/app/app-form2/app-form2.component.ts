496f68bebc125268cfc61fd7f117d7632d72a79a
zul'jin <53787083+6mahdapihka9@users.noreply.github.com>
2020-09-08 08:47:27 +0300

    email validations: required, email

--- src/app/app-form2/app-form2.component.ts
+++ src/app/app-form2/app-form2.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {validate, validateComponent} from 'codelyzer/walkerFactory/walkerFn';
 
 @Component({
   selector: 'app-app-form2',
   templateUrl: './app-form2.component.html',
   styleUrls: ['../app-form.component.css']
 })
-export class AppForm2Component implements OnInit {
-  myForm2: FormGroup;
 
+export class AppForm2Component implements OnInit {
+  // FormGroup объединяет в себе FromControl
+  myForm2: FormGroup;
+  // FormControl — это класс, который позволяет напрямую создавать отдельные элементы ввода и управлять ими.
+  // Конструктор new FormControl() принимает три необязательных параметра:
+  // - изначальное значение данных
+  // - массив валидаторов
+  // - массив асинхронных валидаторов
   constructor() {
     this.myForm2 = new FormGroup({
       username: new FormControl(),
